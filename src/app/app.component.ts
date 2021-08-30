6e21bfef1833f0b1d598281b90c1933368631d5f
aviyot <aviyot@gmail.com>
2021-08-30 11:31:46 +0300

    add opthion to save on localStorge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,30 +30,68 @@ export class AppComponent implements OnInit {
   eventIndex: number;
   futureEvent: ShabatEnteExit;
   recordSize: number;
-  constructor(private http: HttpClient) {}
+  records: ShabatEnteExit[];
+  REQ_DATA = 'resource_id=cfe1dd76-a7f8-453a-aa42-88e5db30d567&limit=400';
+  fromLocal = false;
+  updateDate: Date;
+  ls;
+
+  constructor(private http: HttpClient) {
+    this.ls = window.localStorage;
+  }
 
   ngOnInit() {
-    var records: ShabatEnteExit[];
-    let REQ_DATA = 'resource_id=cfe1dd76-a7f8-453a-aa42-88e5db30d567&limit=400';
+    if (this.ls) {
+      this.loadFromLocal();
+    } else {
+      this.fetchData();
+    }
+  }
+
+  loadFromLocal() {
+    this.records = JSON.parse(this.ls.getItem('shabatHolidyTime'));
+    if (this.records) {
+      this.recordSize = this.records.length;
+      this.futureEventIndex = this.records.findIndex((val) => {
+        return new Date(val.date).getTime() > new Date().getTime() - 86400000;
+      });
+      this.allrecords = this.records;
+      this.eventIndex = this.futureEventIndex;
+      this.futureEvent = { ...this.allrecords[this.futureEventIndex] };
+      this.dataLoaded = true;
+      this.fromLocal = true;
+    }
+  }
+
+  fetchData() {
     this.http
-      .get(`${this.API}${REQ_DATA}`)
+      .get(`${this.API}${this.REQ_DATA}`)
       .subscribe((res: { result: { records: any[] }; success: boolean }) => {
-        records = res.result.records;
-        this.recordSize = records.length;
+        this.fromLocal = false;
+        this.updateDate = new Date();
+        this.records = res.result.records;
+        this.recordSize = this.records.length;
         this.dataLoaded = true;
-        records.sort((a, b) => {
+        this.records.sort((a, b) => {
           return new Date(a.date).getTime() - new Date(b.date).getTime();
         });
-        this.futureEventIndex = records.findIndex((val) => {
+
+        if (this.ls && this.records.length) {
+          this.ls.setItem('shabatHolidyTime', JSON.stringify(this.records));
+        }
+
+        this.futureEventIndex = this.records.findIndex((val) => {
           return new Date(val.date).getTime() > new Date().getTime() - 86400000;
         });
-        this.allrecords = records;
+        this.allrecords = this.records;
         this.eventIndex = this.futureEventIndex;
         this.futureEvent = this.allrecords[this.futureEventIndex];
         //this.records = this.futureRecords.slice(this.pageSize * this.page, this.pageSize * (this.page + 1));;
       });
   }
-
+  manualFetch() {
+    this.fetchData();
+  }
   goNextEvent() {
     if (this.eventIndex < this.recordSize) this.eventIndex++;
     this.futureEvent = this.allrecords[this.eventIndex];
